fd98812c71d26822a779444dd0e7aeec5e999e7d
Martin <mquattrone@fi.uba.ar>
2022-06-11 16:25:59 -0300

    Transaction Module
    Primer approach para crear modulos, struct mutable, funciones y llamarlos desde el main

--- src/main.jl
+++ src/main.jl
@@ -1,1 +1,31 @@
-% test
+"""
+@brief
+
+@authors  Perczyk Francisco
+          Quattrone Martin
+          Sobico Carla
+"""
+
+# Includes
+include("transaction.jl")
+
+# Using
+using .Transaction
+
+# Functions
+function main()
+
+    # Create Object
+    tx_obj = Tx()
+    println(tx_obj.n_tx_in)
+
+    # Change Attribute Value
+    tx_obj.n_tx_in = 4
+    println(tx_obj.n_tx_in)
+
+    # Call function in other module
+    test()
+end
+
+
+main()
--- src/transaction.jl
+++ src/transaction.jl
@@ -9,20 +9,19 @@ module Transaction
 
         n_tx_out::Int64
         outputs_array::Array{Int64}     # TODO: Replace Int64 with Output
+
+        # Default Constructor
+        Tx(    n_tx_in = 0
+            , inputs_array = Array{Int64}[]
+            , n_tx_out = 0
+            , outputs_array = Array{Int64}[]) = new(n_tx_in, inputs_array, n_tx_out, outputs_array)
     end
     export Tx
 
-    # Default Constructor
-    Tx(;   n_tx_in = 0
-         , inputs_array = Array{Int64}[]
-         , n_tx_out = 0
-         , outputs_array =  Array{Int64}[]) = new(n_tx_in, inputs_array, n_tx_out, outputs_array)
-
     # Functions/Methods
-    # function test(tx_in::Tx)
-    #     println("hola")
-    #     println(tx_in.n_tx_in)
-    # end
-    # export test
+    function test()
+        println("hola")
+    end
+    export test
 
 end # module Transaction
