cf9e729423560dd88a6666b6d8b27c1567007759
CarlaSobico <carlisobico@hotmail.com>
2022-06-12 21:09:06 -0300

    block functions

--- src/util.jl
+++ src/util.jl
@@ -1,5 +1,15 @@
 
+import SHA
 
 # Const strings 
 const tx_id_str = "tx_id"
 const idx_str = "idx"
+const prev_block_genesis_str = "F" ^ 32
+const dest_str = "dst"
+const value_str = "value"
+
+# Apply sha256 hash to input string.
+function HashString(str::String)
+    return bytes2hex(SHA.sha256(str))
+end
+export
