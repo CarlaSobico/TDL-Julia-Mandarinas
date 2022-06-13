9a4c54b987c0cdf6af0818bc51bd7b9cd214617b
CarlaSobico <carlisobico@hotmail.com>
2022-06-12 21:13:35 -0300

    refactor format

--- src/input.jl
+++ src/input.jl
@@ -1,23 +1,23 @@
 module InputModule
 
-    # Includes
+# Includes
 
-    # Structs
-    mutable struct Input
-        tx_id::String  # outpoint  Es un hash de la transaccion de donde este input toma fondos
-        idx::Int64; # outpoint //Valor entero no negativo que representa un indice sobre la secuencia de outputs de la transaccion con hash tx id
-        addr::String; # input //La direccion de origen de los fondos (que debe coincidir con la direccion del output referenciado)
-	
-        Input(tx_id = "default_tx_id", idx = 0, addr = "default_addr") = new(tx_id, idx, addr)
-    end
+# Structs
+mutable struct Input
+    tx_id::String  # outpoint  Es un hash de la transaccion de donde este input toma fondos
+    idx::Int64 # outpoint //Valor entero no negativo que representa un indice sobre la secuencia de outputs de la transaccion con hash tx id
+    addr::String # input //La direccion de origen de los fondos (que debe coincidir con la direccion del output referenciado)
 
-    function CompareAddr(input::Input, addr::String) 
-        return cmp(input.addr, addr) == 0
-    end
+    Input(tx_id="default_tx_id", idx=0, addr="default_addr") = new(tx_id, idx, addr)
+end
 
-    function ToString(input::Input) 
-        idx_string = string(input.idx)
-        return string(input.tx_id, " ", idx_string, " ", input.addr, "\n")
-    end
+function CompareAddr(input::Input, addr::String)
+    return cmp(input.addr, addr) == 0
+end
+
+function ToString(input::Input)
+    idx_string = string(input.idx)
+    return string(input.tx_id, " ", idx_string, " ", input.addr, "\n")
+end
 
 end # InputModule
--- src/output.jl
+++ src/output.jl
@@ -1,22 +1,22 @@
 module OutputModule
 
-    # Includes
+# Includes
 
-    # Structs
-    mutable struct Output
-        value::Int64  # La cantidad de Algocoins a transferir en este output
-        addr::String; # La direccion de origen de los fondos (que debe coincidir con la direccion del output referenciado)
+# Structs
+mutable struct Output
+    value::Int64  # La cantidad de Algocoins a transferir en este output
+    addr::String # La direccion de origen de los fondos (que debe coincidir con la direccion del output referenciado)
 
-        Output(value = 0,addr = "default_addr") = new(value, addr)
-    end
+    Output(value=0, addr="default_addr") = new(value, addr)
+end
 
-    function CompareAddr(output::Output, addr::String) 
-        return cmp(output.addr, addr) == 0
-    end
+function CompareAddr(output::Output, addr::String)
+    return cmp(output.addr, addr) == 0
+end
 
-    function ToString(output::Output)
-        value_string = string(output.value)
-        return string(value_string, " ", output.addr, "\n")
-    end
+function ToString(output::Output)
+    value_string = string(output.value)
+    return string(value_string, " ", output.addr, "\n")
+end
 
 end # module Output
